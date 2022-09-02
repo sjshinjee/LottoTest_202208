b3c52cd4512a7c33e166291853e726bafe30dc72
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-02 16:57:18 +0900

    다시 빌드시키려고

---
+++ app/src/main/java/com/example/lottotest_202208/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.lottotest_202208
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var mNumList = ArrayList<Int>()
+    val mNumViewList = ArrayList<TextView>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        numBtn.setOnClickListener {
+           btnNum()
+        }
+
+    }
+
+    fun btnNum(){
+        for (i in 0 until 6) {
+
+            while (true) {
+                val randomNum = (Math.random() * 45 + 1).toInt()
+
+                if(!mNumList.contains(randomNum)) {
+                    mNumList.add(randomNum)
+                    break
+                }
+            }  //whiel(true)
+        }   //for(i in 0 until 6)
+    }
+
+    mNumLi
+
+
+
+}
